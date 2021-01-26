8081c32a50207f8d371490235dc8eaf98f62b85a
Fab001 <44618631+Fab001@users.noreply.github.com>
2021-01-26 11:08:58 +0100

    Update index.js

--- BlobTrigger1/index.js
+++ BlobTrigger1/index.js
@@ -1,31 +1,30 @@
-// Autenticazione computer vision
-const ComputerVisionClient = require('@azure/cognitiveservices-computervision').ComputerVisionClient;
 const ApiKeyCredentials = require('@azure/ms-rest-js').ApiKeyCredentials;
-const keyV = '';
-const endpointV = 'https://visionmaskplease.cognitiveservices.azure.com/';
+
+const ComputerVisionClient = require('@azure/cognitiveservices-computervision').ComputerVisionClient;
+const keyV = process.env["keyVision"];
+const endpointV = process.env["endpointVision"];
 const computerVisionClient = new ComputerVisionClient(new ApiKeyCredentials({ inHeader: { 'Ocp-Apim-Subscription-Key': keyV } }), endpointV);
 
+const FaceClient = require('@azure/cognitiveservices-face').FaceClient;
+const keyF = process.env["keyFaceAPI"];
+const endpointF= process.env["endpointFaceAPI"];
+const clientFace = new FaceClient(new ApiKeyCredentials({ inHeader: { 'Ocp-Apim-Subscription-Key': keyF } }), endpointF);
 
-//Autenticazione Coda
 const { QueueServiceClient } = require("@azure/storage-queue");
-const connStr = "";
+const connStr = process.env["AzureWebJobsStorage"];
 const queueServiceClient = QueueServiceClient.fromConnectionString(connStr);
+const queueName = process.env["queueNAME"];
 
-
-
-//Autenticazione CosmosDB
 const { CosmosClient } = require("@azure/cosmos");
-const endpoint = "https://cosmosmask.documents.azure.com:443/";
-const key = "";
+const endpoint = process.env["endpointCosmos"];
+const key = process.env["keyCosmos"];
 const clientDB = new CosmosClient({ endpoint, key });
 
 
-//Creazione coda per user
-async function createCoda(path, context){
-        context.log("Path della foto: "+path);      
-        let idcoda = path.match(/[^\/?#]+(?=$|[?#])/)[0];
-        context.log("Id della coda creata: "+idcoda);     
-        const queueClient = queueServiceClient.getQueueClient(idcoda);
+//Create queue
+async function createQueue(path){
+        let idqueue = path.match(/[^\/?#]+(?=$|[?#])/)[0];
+        const queueClient = queueServiceClient.getQueueClient(idqueue);
         const createQueueResponse = await queueClient.create();
         return queueClient;
 }
@@ -51,11 +50,50 @@ async function updateCosmos(container){
     }
 }
 
+var maskDetected = false;
+
+// Check with Computer Vision 
+async function checkVision(context, url){
+    context.log('Computer Vision image analysis ...');
+    const caption = (await computerVisionClient.describeImage(url)).captions[0];
+    context.log(`VISION INFO: This may be ${caption.text} (${caption.confidence.toFixed(2)} confidence)`);
+    var descr = caption.text;
+    return descr.includes("mask");
+}
+
+// Check with Face API 
+async function checkFaceAPI(context, url){
+
+    let find = false;
+
+    const options = {
+        returnFaceLandmarks: true,
+        returnFaceAttributes: ["accessories"]
+    }; 
+    let singleDetectedFace = await clientFace.face.detectWithUrl(url, options)
+        .then((result ) => {
+            context.log("FACE API INFO: The result is: ");
+            context.log(result[0].faceAttributes.accessories); 
+            result[0].faceAttributes.accessories.forEach(function(entry) {
+                if(entry.type == "mask"){
+                    find =  true;
+                }  
+            });
+        }).catch((err) => {
+            context.log('No faces detected in single face image:' + url)
+            find = false;
+        })
+
+    return find;
+}
+
+
+
 
 module.exports = async function (context, myBlob) {
-   context.log('FUNZIONE HUB');
 
-   const UrlDaAnalizzare = "https://maskpleasestorage.blob.core.windows.net/"+context.bindingData.blobTrigger;
+   context.log('****** BLOB TRIGGER ****** ');
+   const urlToProcess = process.env["BlobStorage"]+context.bindingData.blobTrigger;
 
     // SETTING COSMOS DB
     const { database } = await clientDB.databases.createIfNotExists({ id: "DBmaskplease" });
@@ -64,33 +102,23 @@ module.exports = async function (context, myBlob) {
     context.log(container.id);
 
 
-    //COMPUTER VISION
-    context.log('Analisi URL immagine da descrivere ...');
-    const caption = (await computerVisionClient.describeImage(UrlDaAnalizzare)).captions[0];
-    context.log(`This may be ${caption.text} (${caption.confidence.toFixed(2)} confidence)`);
-    var descr = caption.text;
-    var check = descr.includes("mask");
+    //Create client queue
+    const queueClient = await createQueue(context.bindingData.blobTrigger); 
 
-
-    //Crea coda per utente
-     const queueClient = await createCoda(context.bindingData.blobTrigger, context); 
-
-    if(check){
-        context.log("MASCHERINA OK");
-        // Invio messaggio alla coda
-        await queueClient.sendMessage("Mascherina OK");
+    if(await checkVision(context, urlToProcess) || await checkFaceAPI(context, urlToProcess)){
+        context.log("OK MASK");
+        // send message to queue
+        await queueClient.sendMessage("OK MASK");
 
         //Update CosmosDB
         await updateCosmos(container);
-    
+
+        return;
     }
     else {
-        context.log("Mascherina non trovata :(");
-        // Invio messaggio alla coda
-        await queueClient.sendMessage("Mascherina non trovata :(");
+        context.log("NO MASK");
+        await queueClient.sendMessage("NO MASK");
         
         }
-
-
 
 };
