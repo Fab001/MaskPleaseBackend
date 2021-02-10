0d67724bab7377d2bb63bc431e4c16c9cadf48f9
Fab001 <44618631+Fab001@users.noreply.github.com>
2021-02-10 16:41:41 +0100

    Update index.js

--- getNumMask/index.js
+++ getNumMask/index.js
@@ -10,16 +10,16 @@ module.exports = async function (context, req) {
 
     //CosmosDB
     const { database } = await clientDB.databases.createIfNotExists({ id: "DBmaskplease" });
-    context.log(database.id);
+   
     const { container } = await database.containers.createIfNotExists({ id: "MaskXday" });
-    context.log(container.id);
+    
 
     var d = new Date();
     var today = new Date().toISOString().slice(0, 10);
 
     
     var queri = "SELECT * FROM c WHERE c.date = '"+today+"'";
-    context.log(queri);
+    
 
     const { resources } = await container.items
     .query(queri)
@@ -32,7 +32,7 @@ module.exports = async function (context, req) {
         };
     }else{
         for (const o of resources) {
-            context.log(`${o.id},${o.numMask}, ${o.date} item catched `);
+            
             context.res = {
                 status: 200, 
                 body: o.numMask
